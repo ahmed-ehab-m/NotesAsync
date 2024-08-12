f54301e564c7de5af49be1192ebb64471873b20c
Ahmed-ehab-coder <ehab02987@gmail.com>
2024-08-12 18:48:15 +0300

    add custom app bar to edit note view

--- lib/views/widgets/custom_icon.dart
+++ lib/views/widgets/custom_icon.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
-
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({super.key, required this.icon});
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -13,7 +13,7 @@ class CustomSearchIcon extends StatelessWidget {
       width: 46,
       height: 46,
       child: Icon(
-        Icons.search,
+        icon,
         size: 28,
       ),
     );
