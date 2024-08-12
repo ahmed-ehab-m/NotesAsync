8112134db5312435d6d7b772eb01c73f8f307c58
Ahmed-ehab-coder <ehab02987@gmail.com>
2024-08-12 18:01:06 +0300

    add fields to bottm sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -13,7 +13,16 @@ class AddNoteBottomSheet extends StatelessWidget {
           SizedBox(
             height: 32,
           ),
-          CustomTextField(),
+          CustomTextField(
+            text: 'Title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            text: 'Content',
+            maxLines: 5,
+          ),
         ],
       ),
     );
